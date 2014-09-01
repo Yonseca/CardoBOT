ed9524a6c0f90278486291ad97cae8b1734b63b3
Alejandro S <yonseca@gmail.com>
2014-09-01 20:50:00 +0200

    Fixed file

--- cambia.py
+++ cambia.py
@@ -1,1 +1,31 @@
-/home/yonseca/git/core/cambia.py
+import pywikibot, re
+from HTMLParser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+
+    save = 0
+    i = 0
+    refs = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag in ['ref']:
+            print "Encountered a start tag:", tag
+            global save, refs, i
+            save = 1
+
+    def handle_endtag(self, tag):
+        print "Encountered an end tag :", tag
+        global save
+        save = 0
+
+    def handle_data(self, data):
+        global save
+        if save == 1:
+            print "Encountered some data  :", data
+
+
+parser = MyHTMLParser()
+site = pywikibot.Site('es', 'wikipedia')
+page = pywikibot.Page(site, 'Amaral')
+#print page.text
+parser.feed(page.text)
