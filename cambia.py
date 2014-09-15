6b42f38014dde6c4d9bcf3c52d8233a77e414079
Alejandro S <yonseca@gmail.com>
2014-09-15 13:40:10 +0200

    Ahora encuentra BIEN las referencias

--- cambia.py
+++ cambia.py
@@ -1,31 +1,53 @@
 import pywikibot, re
-from HTMLParser import HTMLParser
+#~ from HTMLParser import HTMLParser
 
-class MyHTMLParser(HTMLParser):
+#save = 0
 
-    save = 0
-    i = 0
-    refs = []
+#class MyHTMLParser(HTMLParser):
 
-    def handle_starttag(self, tag, attrs):
-        if tag in ['ref']:
-            print "Encountered a start tag:", tag
-            global save, refs, i
-            save = 1
+    ##save = 0
+    #i = 0
+    #refs = []
 
-    def handle_endtag(self, tag):
-        print "Encountered an end tag :", tag
-        global save
-        save = 0
+    #def handle_starttag(self, tag, attrs):
+        #if tag in ['ref']:
+            ##print "Encountered a start tag:", tag
+            #global save, refs, i
+            #save = 1
+            #for attr in attrs:
+                #print "atributo: ", attr
 
-    def handle_data(self, data):
-        global save
-        if save == 1:
-            print "Encountered some data  :", data
+    #def handle_endtag(self, tag):
+        ##print "Encountered an end tag :", tag
+        #global save
+        #save = 0
+
+    #def handle_data(self, data):
+        #global save
+    #savestatus = save
+        ##if savestatus == 1:
+            ##print "Encountered some data  :", data
 
 
-parser = MyHTMLParser()
+#~ parser = MyHTMLParser()
 site = pywikibot.Site('es', 'wikipedia')
 page = pywikibot.Page(site, 'Amaral')
+resul = re.findall("<ref(.*?)</ref>", page.text)
+for reference in resul:
+    reference = "<ref" + reference + "</ref>" + "\n"
+    print reference
+
+dupes = []
+repetitions = 0
+for ref1 in resul:
+    for ref2 in resul:
+        if (ref1 == ref2):
+            repetitions = repetitions + 1
+    if (repetitions == 1):
+        #~ print "Quitado:" + resul.pop(resul.index(ref2)) + "\n"
+        repetitions = 0
+
+
+
 #print page.text
-parser.feed(page.text)
+#~ parser.feed(page.text)
