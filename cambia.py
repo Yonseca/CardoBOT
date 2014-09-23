08c0150740aa19b04acfe267e6041d35bbaa68c4
Alejandro S <yonseca@gmail.com>
2014-09-23 19:30:50 +0200

    MAYDAY, MAYDAY!

--- cambia.py
+++ cambia.py
@@ -5,72 +5,41 @@ from HTMLParser import HTMLParser
 #save = 0
 
 class MyHTMLParser(HTMLParser):
-<<<<<<< HEAD
-=======
-    """ Handles the start ref tag attributes and returns a list with them. 
-        
-        Start tag may have (or not) a name, or a group attribute. Or both!
-        We'll need a name attribute in order to group all references. If
-        it doesn't exist, we must generate one. 
-        We don't need a group attribute, but we should save it if exists, 
-        as we don't want to lose information. 
-        
-    """
-    
-    numRef = 0
->>>>>>> f3e3059... Minor changes
 
     def handle_starttag(self, tag, attrs):
         if tag in ['ref']:
-            print "Econtrado el ref de apertura"
+            print "Encountered a start tag:", tag
             for attr in attrs:
                 print "atributo: ", attr
-<<<<<<< HEAD
-=======
 
     def handle_endtag(self, tag):
         print "Encountered an end tag :", tag
         global save
 
     #def handle_data(self, data):
->>>>>>> 398a7d64ae3baa292ba58be430f8e41cc10fd3ef
 
-    #~ def handle_endtag(self, tag):
-        #~ print "Encountered an end tag :", tag
-        #~ global save
-#~
-    #~ def handle_data(self, data):
 
 parser = MyHTMLParser()
 
 def removeNonDupes(refs):
     """Returns a set containing all distinct duplicated references"""
-
+    
     for i, reference in enumerate(refs):
         if (refs.count(reference) <= 1): #if it appears more than once...
             del refs[i]
     return set(refs)
-
+    
 def groupRefs(refs):
     """TODO: Read a list with duplicated references and groups them using
     the name attribute """
-<<<<<<< HEAD
-    for i, reference in refs:
-        longref = u"<ref>" + reference + u"</ref>"
-        parser.feed(longref)
-        for reference in refs:
-            parser.feed(reference)
-
-=======
     for reference in refs:
         reference = u"<ref>" + reference + u"</ref>"
         parser.feed(reference)
     
->>>>>>> 398a7d64ae3baa292ba58be430f8e41cc10fd3ef
 
 def printRefs(refs):
     """Prints a refs list"""
-
+    
     for reference in enumerate(refs):
         print str(reference) + "\n"
 
@@ -86,7 +55,7 @@ dupes = removeNonDupes(resul)
 
 groupRefs(dupes)
 
-
+    
 printRefs(dupes)
 print str(len(dupes)) + " references to group"
 
